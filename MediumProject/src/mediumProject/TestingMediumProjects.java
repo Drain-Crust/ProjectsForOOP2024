6e50bb6838d86797218683a28782556c6c4f4a28
Adrian Cruz <79950558+Drain-Crust@users.noreply.github.com>
2024-03-12 12:25:29 +1300

    Update TestingMediumProjects.java

--- MediumProject/src/mediumProject/TestingMediumProjects.java
+++ MediumProject/src/mediumProject/TestingMediumProjects.java
@@ -49,40 +49,4 @@ public class TestingMediumProjects {
 		System.out.println(wordCounter);
 	}
 }
-=======
-package mediumProject;
-
-public class TestingMediumProjects {
-	
-	public static void main(String[] args) {
-		topSecretTests();
-		
-		
-		
-	}
-	
-	public static void personalDataTests() {
-		PersonalData person1 = new PersonalData("asfs","fdsfdsf",123);
-		PersonalData person2 = new PersonalData("a222fs","fdasdasd243234fdsf",123);
-		PersonalData person3;
-		person1.setFirstName("fsffsfsffss");
-		System.out.println(person1);
-		person3 = person1.oldestPerson(person2);
-		System.out.println(person3);
-		person2.setAge(200);
-		person3 = person1.oldestPerson(person2);
-		System.out.println(person3);
-	}
-	
-	public static void bankAccountTests() {
-		
-	}
-	
-	public static void topSecretTests() {
-		TopSecret n = new TopSecret("The secret tomato sauce ingredients are: onions, carrots,\r\n"
-				+ "garlic, whole tomatoes, salt, tomato paste and olive oil.");
-		TopSecret s = n.redact(new String[]{"tomato","secret","ol","a"});
-		System.out.println(s);
-	}
-}
 >>>>>>> ee3964c9f9a1026fbc0b8692ba526813bf664a9d:MediumProject/src/mediumProject/TestingMediumProjects.java
