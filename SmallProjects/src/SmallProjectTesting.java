c99f0e022e282e1a65abf658edb7124f7d84ab4a
Adrian Cruz <79950558+Drain-Crust@users.noreply.github.com>
2024-03-15 20:24:21 +1300

    file 3

---
+++ SmallProjects/src/SmallProjectTesting.java
@@ -0,0 +1,43 @@
+package SmallProjects;
+
+public class SmallProjectTesting {
+
+	public static void main(String[] args) {
+		ProjectThreeTests();
+	}
+	
+	public static void ProjectOneTests() {
+		ProjectOne projectOne = new ProjectOne();
+		
+		//ProjectOne.systemMsgs();
+		//projectOne.ComputingUsedFuelAmount();
+		//projectOne.ThreeNumberAverage();
+		//projectOne.convertMilesToKilometers();
+		//projectOne.Division();
+		//projectOne.FahrenheitToCelsius();
+		//projectOne.HowManySeconds();
+		//projectOne.TimeDuration();
+		//ProjectOne.PersonalData.main(null);
+		
+		projectOne.closeKeyboard();
+	}
+	
+	public static void ProjectTwoTests() {
+		//ProjectTwo.Book.main(null);
+		//ProjectTwo.Student.main(null);
+		//ProjectTwo.Person.main(null);
+		//ProjectTwo.NitFlux.main(null);
+		//ProjectTwo.CartesianPoint.main(null);
+		//ProjectTwo.CashPoint.main(null);
+		
+		ProjectTwo.closeKeyboard();
+		
+	}
+	
+	public static void ProjectThreeTests() {
+		//ProjectThree.Grade.main(null);
+		//ProjectThree.Student.main(null);
+		//ProjectThree.Dog.main(null);
+		//ProjectThree.Box.main(null);
+	}
+}
