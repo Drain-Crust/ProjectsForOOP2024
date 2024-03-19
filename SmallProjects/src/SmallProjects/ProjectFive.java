f3193c56b0ffb40248eefd6e5aa5ed13129d0c2b
Adrian Cruz <79950558+Drain-Crust@users.noreply.github.com>
2024-03-19 22:44:59 +1300

    Update ProjectFive.java

--- SmallProjects/src/SmallProjects/ProjectFive.java
+++ SmallProjects/src/SmallProjects/ProjectFive.java
@@ -64,4 +64,97 @@ public class ProjectFive {
 			}
 		}
 	}
+	
+	public class SimpleDate{
+		private int day;
+		private int month;
+		private int year;
+		
+		public SimpleDate(int day, int month, int year) {
+			setDate(day, month, year);
+		}
+		
+		public SimpleDate() {
+			setDate(1, 1, 2000);
+		}
+		
+		public void setDate(int day, int month, int year) {
+			this.setDay(day);
+			this.setMonth(month);
+			this.setYear(year);
+		}
+		
+		public int getDay() {
+			return day;
+		}
+		
+		public void setDay(int day) {
+			this.day = (day >= 1 && day <= 31) ? day : 1;
+		}
+		
+		public int getMonth() {
+			return month;
+		}
+		
+		public void setMonth(int month) {
+			this.month = (month >= 1 && year <= 12) ? month : 1;
+		}
+		
+		public int getYear() {
+			return year;
+		}
+		
+		public void setYear(int year) {
+			this.year = (year >= 2000 && year <= 2024) ? year : 2000;
+		}
+		
+		public String toString() {
+			return this.day + "/" + this.month + "/" + this.year;
+		}
+	}
+	
+	public class SimpleDateApplication{
+		public static void main(String[] args) {
+			ProjectFive projectFive = new ProjectFive();
+			ProjectFive.SimpleDate d1 = projectFive.new SimpleDate();
+			d1.setDay(22);
+			d1.setMonth(3);
+			d1.setYear(2019);
+			System.out.println(d1);
+			SimpleDate d2 = projectFive.new SimpleDate(14,03,2019);
+			System.out.println(d2);
+		}
+	}
+	
+	public class Count{
+		private String value;
+		
+		public Count () {
+			value = "";
+		}
+		
+		public void successor() {
+			this.value += "S";
+		}
+		
+		public void predessor() {
+			char[] cha = this.value.toCharArray();
+			cha[this.value.length()] = ((cha[this.value.length()] == 'S') ? "".charAt(0) : cha[this.value.length()]);
+			this.value = new String(cha);
+		}
+		
+		public String toString() {
+			int count = 0;
+			for (char i : this.value.toCharArray()) {
+				if (i == 'S') {
+					count++;
+				}
+			}
+			return Integer.toString(count);
+		}
+		
+		public void add(Count s) {
+			
+		}
+	}
 }
